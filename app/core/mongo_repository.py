eef1e185a54b4b0817f1ae0c15e35e40a2262868
Enzo-Ezequiel <enzoezequie05@gmail.com>
2026-09-02 12:20:23 -0300

    refactor(repository): extraer helpers de conversion en MongoRepository
    La conversion entre entidad y documento estaba repetida en cinco metodos (id <-> _id). Se centraliza en _to_document y _to_entity, de modo que un cambio en el mapeo se toca en un solo lugar (DRY).

--- app/core/mongo_repository.py
+++ app/core/mongo_repository.py
@@ -17,29 +17,23 @@ class MongoRepository(Repository[T], Generic[T]):
         self.entity_class = entity_class
 
     async def add(self, entity: T) -> T:
-        document = entity.__dict__.copy()
-        document["_id"] = document.pop("id")
-        await self.collection.insert_one(document)
+        await self.collection.insert_one(self._to_document(entity))
         return entity
 
     async def get_by_id(self, entity_id: str) -> T | None:
         document = await self.collection.find_one({"_id": entity_id})
         if document:
-            document["id"] = document.pop("_id")
-            return self.entity_class(**document)
+            return self._to_entity(document)
         return None
 
     async def get_all(self) -> list[T]:
         entities = []
         async for document in self.collection.find():
-            document["id"] = document.pop("_id")
-            entities.append(self.entity_class(**document))
+            entities.append(self._to_entity(document))
         return entities
 
     async def update(self, entity: T) -> T:
-        document = entity.__dict__.copy()
-        document["_id"] = document.pop("id")
-        await self.collection.replace_one({"_id": entity.id}, document)
+        await self.collection.replace_one({"_id": entity.id}, self._to_document(entity))
         return entity
 
     async def delete(self, entity_id: str) -> bool:
@@ -49,6 +43,16 @@ class MongoRepository(Repository[T], Generic[T]):
     async def find_one(self, filters: dict) -> T | None:
         document = await self.collection.find_one(filters)
         if document:
-            document["id"] = document.pop("_id")
-            return self.entity_class(**document)
+            return self._to_entity(document)
         return None
+
+    def _to_document(self, entity: T) -> dict:
+        """Entidad de dominio -> documento Mongo (id -> _id)."""
+        document = entity.__dict__.copy()
+        document["_id"] = document.pop("id")
+        return document
+
+    def _to_entity(self, document: dict) -> T:
+        """Documento Mongo -> instancia de entidad (_id -> id)."""
+        document["id"] = document.pop("_id")
+        return self.entity_class(**document)
